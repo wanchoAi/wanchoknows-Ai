89d2907fe308401f5b7904f8efdc93f951c38a4b
wanchoAi <goatszn17@gmail.com>
2025-08-03 11:09:04 -0400

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        wanchoGreen: "#008000",
+        wanchoGold: "#FFD700"
+      }
+    },
+  },
+  plugins: [],
+}
