3bc90445c4ebcb4a6d6f6f8067186be58a43cbcf
wanchoAi <goatszn17@gmail.com>
2025-08-03 11:18:38 -0400

    Create MemoryToggle.jsx

---
+++ frontend/frontend/src/frontend/src/frontend/src/components/frontend/src/components/frontend/src/components/MemoryToggle.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MemoryToggle({ memoryOn, setMemoryOn }) {
+  return (
+    <div className="mb-4">
+      <label className="inline-flex items-center">
+        <input
